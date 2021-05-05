1fa0c00e6720b69f2ae519708af83083b7ad4fa0
Aseiide <eiseiide@gmail.com>
2021-05-05 23:10:16 +0900

    Xを10に置き換えて、得点を一つの配列に格納

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -7,27 +7,8 @@ scores.each do |n|
   case n
   when 'X'
     shots << 10
-    shots << 0
-  when 'S'
-    shots << 10
   else
     shots << n.to_i
   end
 end
 p shots
-
-frames = []
-shots.each_slice(2) do |n|
-  frames << n
-end
-p frames
-
-point = 0
-frames.each do |frame|
-  if frame.sum == 10
-    point += frame[0] + 10
-  else
-    point += frame.sum
-  end
-end
-p point
