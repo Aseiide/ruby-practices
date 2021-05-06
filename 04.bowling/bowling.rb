02410a9d3459c66b7b520713408b71a032e39c79
Aseiide <eiseiide@gmail.com>
2021-05-06 15:05:42 +0900

    サンプルケースをすべて通過

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -2,21 +2,22 @@
 
 score = ARGV[0]
 scores = score.split(',')
-frames = []
-frame = []
-point = 0
-
-scores.each do |n|
-  n == 'X' ? frame << 10 : frame << n.to_i
-  if frame.count == 1 && frame.first == 10
-    frames << frame
-    frame = []
-  elsif frame.count == 2
-    frames << frame
-    frame = []
+shots = []
+scores.each do |s|
+  if s == 'X'
+    shots << 10
+    shots << 0
+  else
+    shots << s.to_i
   end
 end
 
+frames = []
+shots.each_slice(2) do |s|
+  frames << s
+end
+
+point = 0
 frames.each_with_index do |f, index|
   next_frame = index + 1
   if index <= 8 # 1~9フレーム
@@ -33,16 +34,7 @@ frames.each_with_index do |f, index|
       point += f.sum
     end
   else # 10フレーム
-    binding.irb
     point += f.sum
   end
 end
 p point
-
-# テストケース
-# 6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,6,4,5 134となりダメ 139
-# 6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,X,X 164.正:164
-# 0,10,1,5,0,0,0,0,X,X,X,5,1,8,1,0,4 107.正:107
-# 6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,0,0 134.正:134
-# X,X,X,X,X,X,X,X,X,X,X,X 300.正:300
-# 0,0,0,0,0,0,0,0,0,0,0,0 0 正:0
