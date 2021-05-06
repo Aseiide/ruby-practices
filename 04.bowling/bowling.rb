9af6743e0578c0aaa8e008ab6f5381165d42fd7b
Aseiide <eiseiide@gmail.com>
2021-05-06 10:26:42 +0900

    Xを10と表し、1球でストライクのとき、一個に格納

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -2,16 +2,11 @@
 
 score = ARGV[0]
 scores = score.split(',')
-frame = []
 frames = []
+frame = []
 
 scores.each do |n|
-  if n == 'X'
-    frame << 10
-  else
-    frame << n.to_i
-  end
-
+  n == 'X' ? frame << 10 : frame << n.to_i
   if frame.count == 1 && frame.first == 10
     frames << frame
     frame = []
@@ -19,26 +14,4 @@ scores.each do |n|
     frames << frame
     frame = []
   end
-end
-puts "frames= #{frames}"
-
-# 計算のロジックを書いていく
-# とりあえず1~9フレーム目まで
-point = 0
-frames.each.with_index do |k, i|
-  binding.irb
-  # puts "#{i} #{k}"
-  point += if i != 8 && i != 9
-              if frames[i][0] == 10 && frames[i+1][0] == 10 && frames[i+2][0] == 10
-                30
-              elsif frames[i][0] == 10 && frames[i+1][0] == 10
-                20
-              elsif frames[i][0] == 10
-                10
-              elsif k.sum == 10
-                frames[i+1][0] + 10
-              else
-                k.sum
-              end
-            end
 end
