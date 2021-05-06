ca8827025249d48e697e30d8bc30154b6ab7d292
Aseiide <eiseiide@gmail.com>
2021-05-06 15:11:18 +0900

    rubocopでインデントを修正

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -23,11 +23,11 @@ frames.each_with_index do |f, index|
   if index <= 8 # 1~9フレーム
     if f[0] == 10
       point += 10 + frames[next_frame][0]
-      if frames[next_frame][0] == 10
-        point += frames[next_frame + 1][0]
-      else
-        point += frames[next_frame][1]
-      end
+      point += if frames[next_frame][0] == 10
+                 frames[next_frame + 1][0]
+               else
+                 frames[next_frame][1]
+               end
     elsif f.sum == 10
       point += 10 + frames[next_frame][0]
     else
