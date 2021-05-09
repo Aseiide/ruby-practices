77df3e79a2ed7e6bceb959b5d44bb16a504de39a
Aseiide <eiseiide@gmail.com>
2021-05-09 15:15:21 +0900

    wip

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -14,24 +14,23 @@ end
 
 frames = shots.each_slice(2).to_a
 
-point = 0
-frames.each_with_index.sum do |f, index|
+point = frames.each_with_index.sum do |f, index|
   next_frame = index.next
   if index <= 8 # 1~9フレーム
     if f[0] == 10
-      point += 10 + frames[next_frame][0]
-      point += if frames[next_frame][0] == 10
-                 frames[next_frame + 1][0]
-               else
-                 frames[next_frame][1]
-               end
+      10 + frames[next_frame][0]
+      if frames[next_frame][0] == 10
+        frames[next_frame + 1][0]
+      else
+        frames[next_frame][1]
+      end
     elsif f.sum == 10
-      point += 10 + frames[next_frame][0]
+      10 + frames[next_frame][0]
     else
-      point += f.sum
+      f.sum
     end
   else # 10フレーム
-    point += f.sum
+    f.sum
   end
 end
 p point
