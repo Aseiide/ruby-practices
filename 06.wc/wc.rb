a618c14540441190c4f294bdddddb89221609535
Aseiide <eiseiide@gmail.com>
2021-05-17 23:55:49 +0900

    wip

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 require 'optparse'
-options = ARGV.getopts('l')
+option = ARGV.getopts('l')
+file = ARGV
 
 # オプションなしを実装していく
 text = $stdin.read
@@ -9,3 +10,10 @@ text = $stdin.read
 p text.count("\n")
 p word = text.split(/\s+/).size
 p text.bytesize
+
+p file
+
+# 引数ありのとき
+# 引数に受けたファイルの改行数・単語数・バイト数とtotalをputsする
+
+
