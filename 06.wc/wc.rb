8dadf5d847316580c09640b2ff50466d42b25d70
Aseiide <eiseiide@gmail.com>
2021-05-18 17:06:59 +0900

    wip

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -11,29 +11,24 @@ p word = text.split(/\s+/).size
 p bytes = text.bytesize
 
 # -lオプションの実装
-if option["l"]
-  p lines = text.count("\n")
-end
-# 引数ありのとき
-# 引数に受けたファイルの改行数・単語数・バイト数とfile名をputsする
+p lines = text.count("\n") if option['l']
 
-def has_argv
-  total_lines = 0
-  total_words = 0
-  total_bytes = 0
+# 引数ありの処理
+total_lines = 0
+total_words = 0
+total_bytes = 0
 
-  files = ARGV
-  files.each do |file|
-    content = File.read(file)
-    lines = content.count("\n")
-    words = content.split(/\s+/).size
-    bytes = content.bytesize
+files = ARGV
+files.each do |file|
+  content = File.read(file)
+  lines = content.count("\n")
+  words = content.split(/\s+/).size
+  bytes = content.bytesize
 
-    total_lines += content.count("\n")
-    total_words += content.split(/\s+/).size
-    total_bytes += content.bytesize
+  total_lines += content.count("\n")
+  total_words += content.split(/\s+/).size
+  total_bytes += content.bytesize
 
-    puts "#{lines} #{words} #{bytes} #{file}"
-    puts "#{total_lines} #{total_words} #{total_bytes} total"
-  end
+  puts "#{lines} #{words} #{bytes} #{file}"
+  puts "#{total_lines} #{total_words} #{total_bytes} total"
 end
