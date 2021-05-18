4d0046d8f34f9403c897c8e2851e3af2e9c43b8a
Aseiide <eiseiide@gmail.com>
2021-05-18 16:07:40 +0900

    wip

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -13,11 +13,21 @@ p text.bytesize
 
 # 引数ありのとき
 # 引数に受けたファイルの改行数・単語数・バイト数とfile名をputsする
+total_lines = 0
+total_words = 0
+total_bytes = 0
+
 files = ARGV
 files.each do |file|
   content = File.read(file)
   lines = content.count("\n")
   words = content.split(/\s+/).size
   bytes = content.bytesize
+
+  total_lines += content.count("\n")
+  total_words += content.split(/\s+/).size
+  total_bytes += content.bytesize
+
   puts "#{lines} #{words} #{bytes} #{file}"
+  puts "#{total_lines} #{total_words} #{total_bytes} total"
 end
