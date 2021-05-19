124365bbce6bbabc43fc507037b4d1ab905ae4be
Aseiide <eiseiide@gmail.com>
2021-05-19 11:04:42 +0900

    wip

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -2,12 +2,8 @@
 
 require 'optparse'
 
-option = ARGV.getopts('l')
+@option = ARGV.getopts('l')
 @files = ARGV
-
-total_lines = 0
-total_words = 0
-total_bytes = 0
 
 # 引数がなかった時
 # 標準入力して改行数、単語数、バイト数、ファイル名を出力
@@ -22,27 +18,38 @@ end
 # 引数ありの処理
 def has_argument
   @files.each do |file|
-    content = File.read(file)
-    lines = content.count("\n")
-    words = content.split(/\s+/).size
-    bytes = content.bytesize
-    puts "#{lines} #{words} #{bytes} #{file}"
+    @contents = File.read(file)
+    @lines = @contents.count("\n")
+    @words = @contents.split(/\s+/).size
+    @bytes = @contents.bytesize
+    puts "#{@lines} #{@words} #{@bytes} #{@file}"
   end
-  puts_total if 
+  display_total
 end
 
 # トータルを計算する処理を切り出す
 def calc_total
-  total_lines += content.count("\n")
-  total_words += content.split(/\s+/).size
-  total_bytes += content.bytesize
+  @total_lines = 0
+  @total_words = 0
+  @total_bytes = 0
+  @total_lines += @lines
+  @total_words += @words
+  @total_bytes += @bytes
 end
 
 # トータルを表示する処理
-def puts_total
-    puts total_lines
-  if option["l"] == false
-    puts total_words
-    puts total_bytes
+def display_total
+    print @total_lines
+  if @option["l"] == false
+    print @total_words
+    print @total_bytes
   end
+end
+
+if @files == []
+  non_argument
+else
+  has_argument
+  calc_total
+  display_total
 end
