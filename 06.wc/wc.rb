55d7e2b404fc1de0a3e17643794be5a505546f67
Aseiide <eiseiide@gmail.com>
2021-05-19 12:06:49 +0900

    wip

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -4,48 +4,73 @@ require 'optparse'
 
 @option = ARGV.getopts('l')
 @files = ARGV
-
-# 引数がなかった時
-# 標準入力して改行数、単語数、バイト数、ファイル名を出力
-def non_argument
+if @files == []
   @contents = $stdin.read
-  lines = @contents.count("\n")
-  words = @contents.split(/\s+/).size
-  bytes = @contents.bytesize
-  puts "#{lines} #{words} #{bytes}"
-end
-
-# 引数ありの処理
-def has_argument
+else
   @files.each do |file|
     @contents = File.read(file)
     @lines = @contents.count("\n")
     @words = @contents.split(/\s+/).size
     @bytes = @contents.bytesize
-    puts "#{@lines} #{@words} #{@bytes} #{@file}"
   end
-  display_total
 end
 
-# トータルを計算する処理を切り出す
-def calc_total
-  @total_lines = @total_words = @total_bytes = 0
-  @total_lines += @lines
-  @total_words += @words
-  @total_bytes += @bytes
+# 引数がなしの時
+# 標準入力の改行数、バイト数、単語数を表示
+# lオプションのときだけ改行数を表示する
+if @files == [] && @option['l'] == false
+  @lines = @contents.count("\n")
+  @words = @contents.split(/\s+/).size
+  @bytes = @contents.bytesize
+  puts "#{@lines} #{@words} #{@bytes}"
+elsif @files == [] && @option['l'] == true
+  @lines = @contents.count("\n")
+  puts "#{@lines}"
 end
 
-# トータルを表示する処理
-def display_total
-  print @total_lines
-  print @total_words
-  print @total_bytes
-end
 
-if @files == []
-  non_argument
-else
-  has_argument
-  calc_total
-  display_total
-end
+# def calc_lines_words_bytes
+#   @lines = @contents.count("\n")
+#   @words = @contents.split(/\s+/).size
+#   @bytes = @contents.bytesize
+# end
+
+# def result
+#   if @option['l'] == true
+#     puts @lines
+#   else
+#     print @lines
+#     print @words
+#     print @bytes
+#   end
+# end
+# # 引数がなかった時
+# # 標準入力して改行数、単語数、バイト数、ファイル名を出力
+# def non_argument
+
+#   puts "#{lines} #{words} #{bytes}"
+# end
+
+# # 引数ありの処理
+# def has_argument
+#   @files.each do |file|
+#     @contents = File.read(file)
+#     puts "#{@lines} #{@words} #{@bytes} #{@file}"
+#   end
+#   display_total
+# end
+
+# # トータルを計算する処理を切り出す
+# def calc_total
+#   @total_lines = @total_words = @total_bytes = 0
+#   @total_lines += @lines
+#   @total_words += @words
+#   @total_bytes += @bytes
+# end
+
+# # トータルを表示する処理
+# def display_total
+#   print @total_lines
+#   print @total_words
+#   print @total_bytes
+# end
