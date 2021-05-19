c888d4f6ef78897b84aa3612eb1846013b899fa1
Aseiide <eiseiide@gmail.com>
2021-05-19 13:56:30 +0900

    wip

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -13,32 +13,43 @@ else
   end
 end
 
-def calc_lines_words_bytes
-  @lines = @contents.count("\n")
-  @words = @contents.split(/\s+/).size
-  @bytes = @contents.bytesize
-end
-
-# 引数がなしの時
-if @files == [] && @option['l'] == false
-  calc_lines_words_bytes
-  puts "#{@lines} #{@words} #{@bytes}"
-elsif @files == [] && @option['l'] == true
-  @lines = @contents.count("\n")
-  printf "%7d\n", @lines
-end
-
-# 引数ありの時
-# 引数あり && オプションなし, 引数あり && オプションあり
-if @files != [] && @option["l"] == false
-  @files.each do |file|
-    @contents = File.read(file)
-    calc_lines_words_bytes
-    puts "#{@lines} #{@words} #{@bytes} #{file}"
+def result
+  print @lines
+  unless @option['l']
+    print @lines
+    print @words
+    print @bytes
   end
-  total_lines = total_words = total_bytes = 0
-  total_lines += @lines
-  total_words += @words
-  total_bytes += @bytes
-  puts "#{total_lines} #{total_words} #{total_bytes} total"
+end
+
+def calc_lines_words_bytes
+  @lines = calc_lines(str)
+  @words = calc_words(str)
+  @bytes = calc_bytes(str)
+end
+
+def calc_lines(str)
+  str.count("\n")
+end
+
+def calc_words(str)
+  str.split(/\s+/).size
+end
+
+def calc_bytes(str)
+  str.bytesize
+end
+
+def calc_total
+  @total_lines += @contents.count("\n")
+  @total_words += @contents.split(/\s+/).size
+  @total_bytes += @contents.bytesize
+end
+
+def display_total
+  print @total_lines
+  if @option['l'] == false
+    print @total_words
+    print @total_bytes
+  end
 end
