7a2f1097bd41c399466a08c965fda71eafc4284e
Aseiide <eiseiide@gmail.com>
2021-08-14 11:11:02 +0900

    permissionを表示できるように修正

--- 08.ls_object/file_data.rb
+++ 08.ls_object/file_data.rb
@@ -17,12 +17,18 @@ class FileData
   def initialize(file_path, file_stat)
     @type = file_stat.directory? ? 'd' : '-'
     @nlink = file_stat.nlink
-    @permission = file_stat.mode.to_s(8)[-3, 3].chars
+    @permission = to_permissions(file_stat.mode.to_s(8)[-3, 3])
     @uid = Etc.getpwuid(file_stat.uid).name
     @gid = Etc.getgrgid(file_stat.gid).name
     @size = file_stat.size
     @mtime = file_stat.mtime.strftime('%_m %_d %H:%M')
     @path = file_path
     @blocks = file_stat.blocks
+  end
+
+  private
+
+  def to_permissions(mode)
+    mode.gsub(/[0-7]/, PERMISSIONS)
   end
 end
