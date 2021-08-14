fa90fcf8be328590a17f3907c4cc1938aa2fb734
Aseiide <eiseiide@gmail.com>
2021-08-14 16:05:18 +0900

    WIP

--- 08.ls_object/command.rb
+++ 08.ls_object/command.rb
@@ -1,9 +1,28 @@
-class Command
-  attr_reader :files, :option
+# frozen_string_literal: true
 
-  def initialize(option)
-    @option = option
-    @files = @option['a'] ? Dir.glob('*', File::FNM_DOTMATCH) : Dir.glob('*')
-    @files.reverse! if @option['r']
+require 'optparse'
+require 'etc'
+
+class Command
+  def initialize(a_option: false, l_option: false, r_option: false)
+    @a_option = a_option
+    @l_option = l_option
+    @r_option = r_option
+  end
+
+  def self.call_output
+    options = ARGV.getopts('a', 'l', 'r')
+    command = Command.new(a_option: options['a'], l_option: options['l'], r_option: options['r'])
+    command.output
+  end
+
+  def output
+    file_paths = @a_option ? Dir.glob('*', File::FNM_DOTMATCH) : Dir.glob('*')
+    sorted_file_paths = @r_option ? file_paths.reverse : file_paths
+    file_details = sorted_file_paths.map do |file_path|
+      FileData.new(file_path, File::Stat.new(file_path))
+    end
+    formatter = @l_option ? LongOption.new(file_details) : ShortOption.new(file_details)
+    formatter.print_result
   end
 end
---
+++ 08.ls_object/ls.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative './command'
+require_relative './file_data'
+require_relative './long_option'
+require_relative './short_option'
+
+Command.call_output
